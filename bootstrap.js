044c3a08dd47e6ff5ff6fe149e44a87eed14a7b2
Jason <jason.bautista@sqor.com>
2014-01-09 11:23:38 -0800

    Adding examples of how the framework will work, including basic services

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,9 @@
-// Sqor.js
+// bootstrap.js
 
-
+/**
+ * Simply initializes a few key holder objects.
+ * @return {Object} a shell object for our library, Sqor
+ */
 var initialize = function(){
     var Sqor = {};
     // We define aa few primary holders
@@ -9,8 +12,20 @@ var initialize = function(){
     Sqor.Modules = {};
     Sqor.Services = {};
     Sqor.TP = {};
+
+    return Sqor;
 };
 
 var Sqor = initialize();
 
+// Now we can have services such as:
 
+/*
+    Sqor.Service.HTML
+
+*/
+
+// Example initialize an empty namespace
+(function(Sqor, undefined){
+
+})(Sqor);
