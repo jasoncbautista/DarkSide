5434b8a279835c246f8b4df65fb8b9cc83bb6c76
Jason <jason.bautista@sqor.com>
2014-01-09 11:19:50 -0800

    Clean up whitespace.

---
+++ bootstrap.js
@@ -0,0 +1,16 @@
+// Sqor.js
+
+
+var initialize = function(){
+    var Sqor = {};
+    // We define aa few primary holders
+    Sqor.Core = {};
+    Sqor.Widgets = {};
+    Sqor.Modules = {};
+    Sqor.Services = {};
+    Sqor.TP = {};
+};
+
+var Sqor = initialize();
+
+
