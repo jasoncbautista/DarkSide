9b7a999df2f86f2418174968d15099561931b48e
Jason B <bjason@gmail.com>
2014-01-10 14:07:44 -0800

    Creating basic evener class to use as base for many other classes.

--- bootstrap.js
+++ bootstrap.js
@@ -20,6 +20,116 @@ var initialize = function(){
 };
 var Sqor = initialize();
 
+
+// Eventer.js
+(function(Sqor) {
+    // Dependencies:
+    var $ = Sqor.$;
+    var _ = Sqor._;
+
+    /**
+     * Eventer is a simple publish / subscribe class to help
+     * us create classes that can support siple notifications.
+     *
+     * Usage:
+     *  var eventer = new Eventer();
+     *
+     *  // Client
+     *  eventer.subscribe("all", function() {
+     *      console.log('something changed...');
+     *  });
+     *
+     *  // Server
+     *  eventer.trigger("all");
+     *
+     *
+     * @constructor
+     * @return {Null}
+     */
+    var Eventer = function(){
+        var self = this;
+        self._subscrptions = {};
+    };
+
+    _.extend(Eventer.prototype, {
+
+        /**********************************************************************
+         *   Priavate Methods
+         *********************************************************************/
+
+        /**
+         *  A simple helper to add the event to our event map and return
+         *  a functio to make it easy to unbind.
+         * @param {string} eventName, name of the event to subscribe to
+         * @param {string} id, unieque id associated with handler
+         * @param {function} handler, callback function to trigger on event
+         * @return {function}, unsubscribe function
+         */
+        _addEvent: function(eventName, id, handler) {
+            var self = this;
+            // First we get our subscriptions by eventName
+            var subscriptionsForEvent = self._subscrptions[eventName];
+            // We create a new entry if we don't have any subscriptions
+            if (! _.isReal(subscriptionsForEvent) {
+                self._subscrptions[eventName] = {};
+                subscriptionsForEvent = self._subscrptions[eventName];
+            }
+
+            // Finally, we add our subscription
+            //
+            // WARNING: a very rare problem would be to add two subscriptions
+            // with the same id, potential solution is to loop on
+            // self.subscribe untill a unique is found.
+
+            subscriptionsForEvent[id] = handler;
+            return function(){
+                self._removeSubscription(eventName, id);
+            };
+        },
+
+        _removeSubscription: function(eventName, id) {
+        },
+
+        /**********************************************************************
+         *   Public Methods
+         *********************************************************************/
+
+        /**
+         * The main interface for clents to be able to bind themselves to
+         * events supported by the eventer.
+         *
+         * @param {string} keyName, the name of the event we want to bind to.
+         * @param {function} handler, a callback function to handle the
+         *  triggering
+         *  of the event.
+         * @return {object}, callback to unsubscribe and id for handler
+         */
+        subscribe:  function(eventName, handler) {
+            var self = this;
+            var id = ("" + Math.random()).replace(".", "_");
+            // Delegate the addition of the actual subscription to a helper
+            var unsubscribeCallback = _addEvent(eventName, id, handler);
+            return {
+                unsubscribe: unsubscribeCallback
+                id: id
+            };
+        },
+
+        /**
+         * Get all the functions subscribed to this event and call them.
+         *
+         * @param {string} eventName, the name of the event to trigger
+         * @return {Null}
+         */
+        trigger: function(eventName) {
+        },
+
+        // Workaround for annoying last comma rule.
+        sdfsd3423452349249239493234: null
+    });
+
+    Sqor.Core.Eventer
+})(Sqor);
 // Data.js
 (function(Sqor) {
     // Dependencies:
@@ -159,10 +269,10 @@ var Sqor = initialize();
             }, 100); // TODO: REMOVE, this is for demo porpuposes.
         },
 
+        // Workaround for annoying last comma rule.
         sdfsd3423452349249239493234: null
     });
     Sqor.Widgets.DisplayCard = DisplayCard;
 })(Sqor);
 
 
-// Let's make a list widget / module:
