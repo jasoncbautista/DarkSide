156f23589eac6acb19546700c33bd192cc92a525
Jason <bjason@gmail.com>
2014-01-15 18:47:28 -0800

    Adding delegate

--- bootstrap.js
+++ bootstrap.js
@@ -628,20 +628,18 @@ var Sqor = initialize();
     var SmartTable = function(){
     };
 
-    SmartTable.test = function(count) {
+
+    SmartTable.test = function(count, dataDelegate) {
         var addListener = function(elements) {
             var elementsUp = 0;
             var rearrangeElements= function(elementsArray, elementsUp) {
                     var displayAreaHeight = window.innerHeight;
                     var middleElement = elementsArray[Math.floor(elementsArray.length *  1/3)];
-
                     middleElementTop = middleElement.offset().top -
                         $(window).scrollTop();
-
                     var middleElementRealTop = (middleElementTop + middleElement.height() );
                     console.log('middleElementRealTop', middleElementRealTop);
                     console.log('displayAreaHeight/2', displayAreaHeight/2);
-
                     if ( middleElementRealTop  <= displayAreaHeight/2) {
                         // Now we move one of our elements from the head to the
                         // tail
@@ -656,10 +654,7 @@ var Sqor = initialize();
                         elementsArray.unshift(tail);
                         elementsUp--;
                         console.log('shift down..');
-
                     }
-
-
                 return [elementsArray, elementsUp];
             };
 
@@ -684,7 +679,6 @@ var Sqor = initialize();
                 console.log('eleementsUp', results[1]);
                 domElements = results[0];
                 elementsUp = results[1];
-
             });
         };
         var self = this;
@@ -699,9 +693,7 @@ var Sqor = initialize();
             var newEl = $("<div class='fixed'>" + ii + "</div>");
             parentEl.append(newEl);
             domElements.push(newEl);
-
             var height = newEl.height();
-
             // TODO: start this at - 1/3 hidden away
             newEl.css("top", height * ii + "px");
         }
@@ -718,7 +710,25 @@ var Sqor = initialize();
 })(Sqor);
 
 $(document).ready(function(){
-    Sqor.Widgets.SmartTable.test(15);
+    var dataDelegate = function(count) {
+        var self = this;
+        this._count = count;
+    };
+
+    dataDelegate.prototype.cellCount = function() {
+        var self = this;
+        return self._count;
+    };
+
+    dataDelegate.prototype.cellAtIndex = function(index){
+        return $("<div><h2>" + index + "</h2></div>");
+    };
+
+    Sqor.Widgets.SmartTable.test(25);
 });
+
+
+
+
 
 
