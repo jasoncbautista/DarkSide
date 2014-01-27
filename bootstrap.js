e794e9a8f46d9753cf6ac7010b19257498790f0e
Jason <jason.bautista@sqor.com>
2014-01-27 11:03:23 -0800

    Implemented skeleton for model class.

--- bootstrap.js
+++ bootstrap.js
@@ -272,25 +272,108 @@ setupSettings(Sqor);
     var $ = Sqor.$;
     var _ = Sqor._;
     var Messenger = Sqor.Services.Messenger;
-
+    var Eventer = Sqor.Core.Eventer;
+    // TODO: switch away from static model
     /**
      * @constructor
      * @param {type} options,
      * @return {Null}
      */
     var Model = function(options){
+        var self = this;
+        var defaults = {
+                path: "/"
+            , connectionType: "REST"
+            , isLocalOnly: true
+            , atuosave: true
+            , data: {}
+        };
 
+        self._options = _.extend({}, defaults, options);
+        self.create();
     };
 
-    Model.prototype = new Sqor.Core.Eventer();
+    // We subclass off our eventer class
+    Model.prototype = new Eventer();
 
     _.extend(Model.prototype, {
         // TODO:
         // get("key", "defaultValue");
         // var title = model.get("title", "");
 
+        create: function() {
+            var self = this;
+            // Load our data
+            self._loadData(self._options.data);
+        },
+
+
+        save: function(newProperties){
+            // TODO: implement
+        },
+
+        // TODO: overwrite subscribe to accept things like:
+        //  set:PropertyName   delete:PropertyName
+
+
+        /**
+         * Whenever there is a set on a given property, we notify even
+         * if they are the same value. We also save by default.
+         * @param {type} propertyName,
+         * @param {type} value,
+         * @return {Null}
+         */
+        set: function(propertyName, value /*, notifyOnlyIfDifferent*/) {
+            var self = this;
+            self._rawData[propertyName] =  value;
+            if (!self._options.isLocalOnly) {
+                self.save();
+            }
+            self.trigger(propertyName, {data: self._rawData, newValue: value});
+        },,
+
+        /**
+         * Simple function to help load our data into local varible
+         * @param {type} data,
+         * @param {type} shouldNotify,
+         * @return {Null}
+         */
+        _loadData: function(data, shouldNotify){
+            var self = this;
+            self._rawData = data;
+            if (shouldNotify) {
+            }
+        },
+        // Workaround for annoying last comma rule.
+        sdfsd3423452349249239493234: null
     });
 
+    _.extend(Model, {
+    });
+    Sqor.Core.Model = Model;
+})(Sqor);
+
+
+// SimpleCollection.js
+(function(Sqor) {
+    // Dependencies:
+    var $ = Sqor.$;
+    var _ = Sqor._;
+    var Messenger = Sqor.Services.Messenger;
+    var Eventer = Sqor.Core.Eventer;
+    var Model = Sqor.Core.Model;
+
+    var SimpleCollection = function(options){
+        var self = this;
+        var defaults = {
+            model: Sqor.Models.Generic
+        };
+    };
+
+    _.extend(SimpleCollection, {
+    });
+
+    Sqor.Core.SimpleCollection = SimpleCollection;
 })(Sqor);
 
 // Collection.js
@@ -682,6 +765,8 @@ setupSettings(Sqor);
     var _ = Sqor._;
 
     var ExampleGridController = function(options){
+        var self = this;
+        self.create(options):
     };
 
     _.extend(ExampleGridController.prototype, {
@@ -690,7 +775,7 @@ setupSettings(Sqor);
          * Siple create function to setup model and view along with delegates.
          * @return {Null}
          */
-        create: function(){
+        create: function(options){
             var self = this;
             self._model = new Sqor.Modules.FeedListModel();
             var gridViewOptions = {
@@ -712,6 +797,9 @@ setupSettings(Sqor);
         sdfsd3423452349249239493234: null
     });
 })(Sqor);
+
+// ExampleGridModel.js
+
 
 // SimpleGrid.js
 (function(Sqor) {
