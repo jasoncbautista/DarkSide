1a5e4ebd1d4462e48e898c949d4a4fedb9405819
Jason <jason.bautista@sqor.com>
2014-01-28 13:52:33 -0800

    Showing power of grid

--- bootstrap.js
+++ bootstrap.js
@@ -931,7 +931,7 @@ setupSettings(Sqor);
             // return displayCard.getDomElement();
             var options = {
                 title: model.name
-                , subtitle: "" //model.division + " - " + model.location
+                , subtitle: model.first_name + " " + model.last_name
                 , author: ""
                 , imageURI: ""
                 , externalURI: ""
