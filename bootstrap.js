4e9377c749da5d17afc0785ce315fe20881d11fa
Jason <jason.bautista@sqor.com>
2014-01-29 11:41:17 -0800

    Moving our samples to inside a demoRoutes method.

--- bootstrap.js
+++ bootstrap.js
@@ -64,6 +64,96 @@ Sqor.demoRoutes = function(){
     // Subscribe to a few routes to see what we can do
 
     // TODO(Jason): each module should subscribe itself to these
+
+
+    /**
+     * Quickwa to call our experimental smart dynamic table
+     * @return {null}
+     */
+    var runComplexTable  = function() {
+        var dataDelegate = function(count) {
+            var self = this;
+            this._count = count;
+        };
+
+        dataDelegate.prototype.cellCount = function() {
+            var self = this;
+            return self._count;
+        };
+
+        /**
+         * A simple dummy delegate method used to test our table.
+         * @param {type} index,
+         * @return {null}
+         */
+        dataDelegate.prototype.cellAtIndex = function(index){
+            return $("<div><h2>" + index + "</h2></div>");
+        };
+        Sqor.Widgets.SmartTable.test(50);
+    };
+
+    /**
+     * Helper function to load our dynamic table module.
+     * It basically auto loads more  models to do infinite scroll
+     * @return {null}
+     */
+    var runSimpleDynamicTableModule =  function() {
+        var c = new Sqor.Modules.FeedListController();
+        $("body").append(c.getDomElement());
+        //append to Model
+        // c._model.appendItems(10);
+        // c._model.prepend(10);
+        window._c = c;
+    };
+
+    var runSimpleGrid = function(count) {
+
+        var dataDelegate = {
+            getNumberOfCells: function() {
+                return count;
+            },
+
+            getCellAtIndex:  function(index){
+                return $("<span class='SQOR_fakeCell'>" + index + "</span>");
+            }
+        };
+
+        /**
+         * A simple dummy delegate method used to test our table.
+         * @param {type} index,
+         * @return {null}
+         */
+        var options = {
+            dataDelegate: dataDelegate
+        };
+        var grid = new Sqor.Widgets.SimpleGrid(options);
+        $("body").append(grid.getDomElement());
+    };
+
+    var runDataGrid = function(showTeams){
+        var playerModelOptions = {
+                    path: "/sports/players"
+                ,   fetchAll: true
+                ,   urlParams: {
+                        team_id: 32043
+                    ,   limit: 30000
+                }
+        };
+        if (showTeams) {
+            playerModelOptions = {};
+         }
+
+        var c = new Sqor.Modules.ExampleGridController({modelOptions: playerModelOptions});
+        $("body").append(c.getDomElement());
+        //append to Model
+        // c._model.appendItems(10);
+        // c._model.prepend(10);
+        window._c = c;
+
+    };
+
+    // Now we bind to our router...
+
 };
 
 // Eventer.js
@@ -1956,90 +2046,6 @@ Sqor.demoRoutes = function(){
 })(Sqor);
 
 $(document).ready(function(){
-
-    /**
-     * Quickwa to call our experimental smart dynamic table
-     * @return {null}
-     */
-    var runComplexTable  = function() {
-        var dataDelegate = function(count) {
-            var self = this;
-            this._count = count;
-        };
-
-        dataDelegate.prototype.cellCount = function() {
-            var self = this;
-            return self._count;
-        };
-
-        /**
-         * A simple dummy delegate method used to test our table.
-         * @param {type} index,
-         * @return {null}
-         */
-        dataDelegate.prototype.cellAtIndex = function(index){
-            return $("<div><h2>" + index + "</h2></div>");
-        };
-        Sqor.Widgets.SmartTable.test(50);
-    };
-
-    /**
-     * Helper function to load our dynamic table module.
-     * It basically auto loads more  models to do infinite scroll
-     * @return {null}
-     */
-    var runSimpleDynamicTableModule =  function() {
-        var c = new Sqor.Modules.FeedListController();
-        $("body").append(c.getDomElement());
-        //append to Model
-        // c._model.appendItems(10);
-        // c._model.prepend(10);
-        window._c = c;
-    };
-
-    var runSimpleGrid = function(count) {
-
-        var dataDelegate = {
-            getNumberOfCells: function() {
-                return count;
-            },
-
-            getCellAtIndex:  function(index){
-                return $("<span class='SQOR_fakeCell'>" + index + "</span>");
-            }
-        };
-
-        /**
-         * A simple dummy delegate method used to test our table.
-         * @param {type} index,
-         * @return {null}
-         */
-        var options = {
-            dataDelegate: dataDelegate
-        };
-        var grid = new Sqor.Widgets.SimpleGrid(options);
-        $("body").append(grid.getDomElement());
-    };
-
-    var runDataGrid = function(){
-        var playerModelOptions = {
-                    path: "/sports/players"
-                ,   fetchAll: true
-                ,   urlParams: {
-                        team_id: 32043
-                    ,   limit: 30000
-                }
-        };
-        // var playerModelOptions = {};
-
-        var c = new Sqor.Modules.ExampleGridController({modelOptions: playerModelOptions});
-        $("body").append(c.getDomElement());
-        //append to Model
-        // c._model.appendItems(10);
-        // c._model.prepend(10);
-        window._c = c;
-
-    };
 
     //runSimpleGrid(13);
     //runSimpleDynamicTableModule();
