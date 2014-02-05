e0e0ac2acfc4542a15a9dc17d89c43060c59e214
Jason <bjason@gmail.com>
2014-02-04 20:21:52 -0800

    Added core delegate methods.

--- src/core/delegatorBase.js
+++ src/core/delegatorBase.js
@@ -5,6 +5,39 @@
     var _ = Sqor._;
 
     var DelegatorBase = function(){
+        var self = this;
+        self._delegates = [];
     };
+
+    _.extend(DelegatorBase, {
+
+        /**
+         * Adds a delegate to our list of delegates
+         * @param {object} delegate,
+         * @return {null}
+         */
+        addDelegate: function(delegate){
+            var self = this;
+            self._delegates.push(delegate);
+        },
+
+        /**
+         * Calls all delegates listening for a given method
+         * @return {null}
+         */
+        _notifyDelegates: function(methodName, _arguments){
+            var self = this;
+            var args = _arguments;
+            _.each(self._delegates, function(delegate) {
+                if (_.isReal(delegate[methodName])) {
+                    delegate[methodName].apply(delegate, args);
+                }
+            });
+        },
+
+        // Workaround for annoying last comma rule.
+        sdfsd3423452349249239493234: null
+
+    });
 
 })(Sqor);
